882e3c839bfa105334b5a623dc7c95cb38c5bc4f
mcalder113 <33291529+mcalder113@users.noreply.github.com>
2025-07-18 14:38:11 -0500

    Create jsonDataframe.py

---
+++ jsonDataframe.py
@@ -0,0 +1,12 @@
+import json
+import pandas as pd
+
+with open('data.json', 'r') as json_data:
+    data = json.load(json_data)
+
+#normalize json
+normalized_data = pd.json_normalize(data['views'])
+
+df = pd.DataFrame(normalized_data)
+
+df
