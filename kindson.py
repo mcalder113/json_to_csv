329b4c0a3420688e5c1f6517f27cd4bd38628350
mcalder113 <33291529+mcalder113@users.noreply.github.com>
2025-07-28 14:01:40 -0500

    Update kindson.py

--- kindson.py
+++ kindson.py
@@ -1,7 +1,7 @@
 import json
 import pandas as pd
 
-with open('data.json', 'r') as json_data:
+with open('views.json', 'r') as json_data:
 	data = json.load(json_data)
 
 for i in range(0, len(data)):
